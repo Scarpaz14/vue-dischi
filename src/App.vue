b86fff7d19e6729f9dbf4aaa0bfab8d713530841
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-07 16:31:52 +0200

    aggiunto e ordinato cartelle dei vari blocchi e contenitori

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
+    <HeaderComponent/>
+    <MainComponent/>
   </div>
 </template>
 
 <script>
 
+import HeaderComponent from './components/blocks/HeaderComponent.vue';
+import MainComponent from './components/blocks/MainComponent.vue';
+
 
 export default {
   name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+  }
+  
  
 }
 </script>
 
 <style lang="scss">
+@import './assets/style/global.scss';
 
 
 </style>
