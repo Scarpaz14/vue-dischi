fc21bb0323119542a990e22b51229944aa459853
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-08 16:33:59 +0200

    aggiunto elemento select (senza logica)

---
+++ src/components/commons/SelectGenre.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+        <label for="genre">Fascia d'età</label>
+        <select id="genre" v-model="dataSelect.genreSelected">
+            <option value="Rocks">Rocks</option>
+            <option value="Pop">Pop</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import dataSelect from '../../shared/dataSelect'
+export default {
+    name: 'SelectGenre',
+    data(){
+        return{
+            dataSelect,
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
